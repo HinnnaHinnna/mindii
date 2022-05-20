d8951a65f84484e384f50d613a8f4035f1b5c4e5
HinnnaHinnna <103890709+HinnnaHinnna@users.noreply.github.com>
2022-05-20 20:06:36 +0900

    Add files via upload

---
+++ sketch_con2.js
@@ -0,0 +1,24 @@
+let myFont;
+function preload() {
+  myFont = loadFont();
+}
+let str = div#article.innerText;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textSize(40);
+}
+
+function draw() {
+  background(220);
+
+  let unicode = '';
+  for (var i = 0; i < str.length; i++) {
+    let unitmp = str[i].charCodeAt(0).toString(16);
+    let uniint = parseInt(unitmp,16) + 1;
+    unicode += String.fromCharCode(uniint);
+  }
+  str = unicode;
+
+  fill(0);
+  text(str, 100, 240);
+}
